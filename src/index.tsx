e9e394c5e23ee6eb92db2aae523af75fd152414c
Mihail Ujanin <MihailUj@yandex.ru>
2025-10-06 23:06:02 +0300

    Выполнение работы

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -12,20 +12,38 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+export interface FormData {
+	fontFamily: string;
+	fontSize: string;
+	fontColor: string;
+	backgroundColor: string;
+	contentWidth: string;
+}
+
 const App = () => {
+	const [customStyles, setCustomStyles] = useState<FormData>({
+		fontFamily: defaultArticleState.fontFamilyOption.value,
+		fontSize: defaultArticleState.fontSizeOption.value,
+		fontColor: defaultArticleState.fontColor.value,
+		backgroundColor: defaultArticleState.backgroundColor.value,
+		contentWidth: defaultArticleState.contentWidth.value,
+	});
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': customStyles.fontFamily,
+					'--font-size': customStyles.fontSize,
+					'--font-color': customStyles.fontColor,
+					'--container-width': customStyles.contentWidth,
+					'--bg-color': customStyles.backgroundColor,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				onFormDataSubmit={(data: FormData) => setCustomStyles(data)}
+			/>
 			<Article />
 		</main>
 	);
